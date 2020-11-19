c6771defeb97d7fee73d71cf5a5721f768aa9357
Rasmus Højte <rasmusvh@msn.com>
2020-11-19 23:19:49 +0100

    Add initial rational response exercise

--- cbt-ui/src/containers/RationalResponseExercise.tsx
+++ cbt-ui/src/containers/RationalResponseExercise.tsx
@@ -1,9 +1,30 @@
 import React from "react";
 
 export function RationalResponseExercise(props: any) {
-    const [exerciseState, setExerciseState] = React.useState({});
-    console.log("asdadsd");
+    const [exerciseState, setExerciseState] = React.useState<Array<RationalResponse>>([]);
+    const [statementState, setStatementState] = React.useState<string>("");
+    const [responseState, setResponseState] = React.useState<string>("");
+
+    const pushNewExercise = (response: RationalResponse) => {
+        setExerciseState([...exerciseState, response]);
+    }
+
+    let previousStatements = exerciseState.map((response) => {
+        return <div>
+            <span style ={{marginRight:"50px"}}>{response.statement}</span>
+            <span>{response.reply}</span>
+        </div>
+    })
+
     return (<div>
-        asdasdasdasdasd
+        {previousStatements}
+        <input onChange={(e) => setStatementState(e.target.value)} type="text" style ={{marginRight:"50px"}} />
+        <input onChange={(e) => setResponseState(e.target.value)} type="text" />
+        <button onClick={() => pushNewExercise({ statement: statementState, reply: responseState})}>Enter</button>
     </div>)
+}
+
+interface RationalResponse {
+    statement: string;
+    reply: string;
 }
