dbd54b9795d7f9e96cd254314848f7953678db74
Rasmus Højte <rasmusvh@msn.com>
2020-12-31 00:15:40 +0100

    Add link on hamburger icon

--- cbt-ui/src/components/navigation/NavBar.tsx
+++ cbt-ui/src/components/navigation/NavBar.tsx
@@ -7,6 +7,9 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import { LoginButton } from '../LoginButton';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import { StyledLink } from '../styled/Navigation';
 
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -26,13 +29,44 @@ const useStyles = makeStyles((theme: Theme) =>
 export const NavBar = () => {
   const classes = useStyles();
 
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+      setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+      setAnchorEl(null);
+  };
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}>
             <MenuIcon />
           </IconButton>
+          <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+                open={open}
+                onClose={handleClose}
+            >
+                <MenuItem onClick={handleClose}><StyledLink to="/">Home</StyledLink></MenuItem>
+            <MenuItem onClick={handleClose}><StyledLink to="/exercises">Exercises</StyledLink></MenuItem>
+            </Menu>
           <Typography variant="h6" className={classes.title}>
             MyCBT
           </Typography>
